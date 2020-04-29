1a4c38a30d69656259d92589289d2390a8523af1
Hyelin Seok <50590132+Shyellen@users.noreply.github.com>
2020-04-29 23:01:24 +0900

    conversion.py: remove unnecessary lines

--- py_lab2/my_pkg/conversion.py
+++ py_lab2/my_pkg/conversion.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 
 def toOctal(bnum):
-    bnum = '0b'+bnum
     num = int(bnum, 2)
     #onum = oct(num)
     return format(num, 'o')
 
 def toDecimal(bnum):
-    bnum = '0b'+bnum
     dnum = int(bnum, 2)
     return dnum
 
 def toHexadecimal(bnum):
-    bnum = '0b'+bnum
     num = int(bnum, 2)
     #hnum = hex(num)
     return format(num, 'X')
